8aed34b5df319abdba1cf10a82a148203c9aa0c4
unknown <fundimsafiri6@gmail.com>
2026-06-13 16:11:28 +0300

    Add render config and fix settings

--- ai_complaints_system/settings.py
+++ ai_complaints_system/settings.py
@@ -118,25 +118,24 @@ WSGI_APPLICATION = 'ai_complaints_system.wsgi.application'
 
 
 import dj_database_url
+import os
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('MYSQL_URL'),
-        conn_max_age=600,
-        ssl_require=False
-    )
-}
+database_url = os.environ.get("DATABASE_URL")
 
-# Fallback for local development
-if not os.environ.get('MYSQL_URL'):
+if database_url:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": dj_database_url.parse(
+            database_url,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
